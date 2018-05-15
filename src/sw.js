2cea72ec894e3db115ca25ebe42c563afbbae3ee
jigsaw <m@jgs.me>
2018-05-15 17:27:57 +0900

    Clean

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,4 @@
 /* global workbox */
-
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js'
-)
-
 workbox.routing.registerRoute(/.*\.js$/, workbox.strategies.cacheFirst())
 
 workbox.routing.registerRoute(/.*\.css$/, workbox.strategies.cacheFirst())
